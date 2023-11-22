9f494dae4ad54d04bf98b55dfceeb8f083da2914
sere06001 <sere06001@utb.vaxjo.se>
2023-11-22 17:11:53 +0100

    Add project files.

---
+++ Inlämnings Uppgift 5.5 C-A/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Uppgift5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Detta program fungerar som en Magic 8-ball, ställ en fråga! Programmet avslutas om du skriver in 'a'");
+            string fråga ="";
+            string aaa = "a";
+            string[] svar = new string[10] { "Ja", "Nej", "Kanske", "Definitivt", "Definitivt inte", "Antagligen", "Antagligen inte", "Utan tvekan", "Räkna inte på det", "Det kan du räkna med" };
+            while (fråga != "a")
+            {
+                fråga = Console.ReadLine();
+                Random random= new Random();
+                if (fråga == "a")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(svar[random.Next(11)]);
+                    Console.WriteLine();
+                    Console.WriteLine("Fråga igen!");
+                }
+            }
+        }
+    }
+}
